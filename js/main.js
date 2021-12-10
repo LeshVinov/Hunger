2f31dfff28f35fa203f96484ba472a703f96447e
LeshVinov <90595946+LeshVinov@users.noreply.github.com>
2021-12-10 15:36:02 +0300

    done_without_adaptation

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const linkDown = document.querySelectorAll('.link_down');
+// const secondSection = document.querySelector('.section_about-us')
+// 
+// const trackScroll = () => {
+// 
+    // window.scrollBy(0, secondSection.offsetTop)
+// 
+// }
+// 
+// linkDown.addEventListener('click', (e) => {
+    // e.preventDefault()
+    // trackScroll()
+// }) Анимация прокрутки на сайте
+
+
+
+
+// const forms = document.querySelectorAll('form')
+// 
+// let formId
+// 
+// const sendForm = (formId) => {
+    // const form = document.getElementById(formId)
+// 
+    // console.log(form);
+// }
+// 
+// 
+// forms.forEach((form, id) => {
+    // form.addEventListener('submit', (event) => {
+        // event.preventDefault()
+        // formId = `form${id+1}`
+        // sendForm(formId)
+    // })
+// }) 28:20 отправка формы
